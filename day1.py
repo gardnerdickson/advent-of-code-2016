eb2ccefc99536ad687f93d2f457986c0a65c4579
Gardner Dickson <gardner.dickson@gmail.com>
2019-10-23 11:17:42 -0400

    Add day1, part 2

--- day1.py
+++ day1.py
@@ -1,10 +1,24 @@
+from dataclasses import dataclass
 from enum import Enum, auto
+
 
 class Direction(Enum):
     NORTH = auto()
     EAST = auto()
     SOUTH = auto()
     WEST = auto()
+
+
+@dataclass(frozen=True, eq=True)
+class Point:
+    x: int
+    y: int
+
+    def __str__(self):
+        return "x: {0}, y: {1}".format(self.x, self.y)
+
+    def move(self, x, y):
+        return Point(self.x + x, self.y + y)
 
 
 class Position:
@@ -23,35 +37,67 @@ class Position:
         Direction.WEST: DirectionInfo(Direction.SOUTH, Direction.NORTH, -1, 0)
     }
 
-    def __init__(self, x, y, direction):
-        self.x = x
-        self.y = y
+    def __init__(self, location, direction):
+        self.location = location
         self.direction = direction
 
     def __str__(self):
-        return "x: {0}, y: {1}, direction: {2}".format(self.x, self.y, self.direction)
+        return "location: {0}, direction: {1}".format(self.location, self.direction)
 
     def move(self, turn, amount):
         if turn == 'L':
-            self.direction = Position.direction_info_table[self.direction].next_left
+            direction = Position.direction_info_table[self.direction].next_left
         else:
-            self.direction = Position.direction_info_table[self.direction].next_right
-        self.x += amount * Position.direction_info_table[self.direction].x_multiplier
-        self.y += amount * Position.direction_info_table[self.direction].y_multiplier
+            direction = Position.direction_info_table[self.direction].next_right
+        location = self.location.move(
+            amount * Position.direction_info_table[direction].x_multiplier,
+            amount * Position.direction_info_table[direction].y_multiplier
+        )
+        return Position(location, direction)
+
+    def range(self, position):
+        if self.location.x == position.location.x:
+            step = 1 if self.location.y < position.location.y else -1
+            points = map(lambda y: Point(self.location.x, y), range(self.location.y + (1 * step), position.location.y + (1 * step), step))
+        elif self.location.y == position.location.y:
+            step = 1 if self.location.x < position.location.x else -1
+            points = map(lambda x: Point(x, self.location.y), range(self.location.x + (1 * step), position.location.x + (1 * step), step))
+        else:
+            raise RuntimeError("Positions are not aligned")
+        return list(points)
 
 
-def day_1(input_file):
+def day_1_1(input_file):
     file = open(input_file, "r")
     instructions = file.read().split(", ")
     file.close()
-    current_position = Position(0, 0, Direction.NORTH)
+    current_position = Position(Point(0, 0), Direction.NORTH)
     for (turn, amount) in parse(instructions):
-        current_position.move(turn, amount)
-    print(abs(current_position.x) + abs(current_position.y))
+        current_position = current_position.move(turn, amount)
+    return abs(current_position.location.x) + abs(current_position.location.y)
 
+
+def day_1_2(input_file):
+    file = open(input_file, "r")
+    instructions = file.read().split(", ")
+    file.close()
+    current_position = Position(Point(0, 0), Direction.NORTH)
+    visited = set()
+    for (turn, amount) in parse(instructions):
+        new_position = current_position.move(turn, amount)
+        locations = current_position.range(new_position)
+        for location in locations:
+            if location in visited:
+                return abs(location.x) + abs(location.y)
+            visited.add(location)
+        current_position = new_position
+    return None
 
 def parse(instructions):
     return map(lambda instruction: (instruction[:1], int(instruction[1:])), instructions)
 
 
-day_1("input1.txt")
+answer_1 = day_1_1("input1.txt")
+print("Part 1: " + str(answer_1))
+answer_2 = day_1_2("input1.txt")
+print("Part 2: " + str(answer_2))
